7db66a17e03badfb2fe5bd56f300be39d13ed657
soumith <soumithganji@gmail.com>
2020-05-20 23:32:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hitch/splashscreen.java
@@ -0,0 +1,21 @@
+package com.example.hitch;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
+
+
+
